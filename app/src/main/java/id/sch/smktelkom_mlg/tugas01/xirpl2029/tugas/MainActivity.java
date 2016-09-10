414495bf153ea735616ac5d433a1f3c741661a77
Chrisdaniar <ct.rputri@yahoo.com>
2016-09-10 20:22:28 +0700

    baru lagi scriptnya

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl2029/tugas/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl2029/tugas/MainActivity.java
@@ -1,90 +1,72 @@
 package id.sch.smktelkom_mlg.tugas01.xirpl2029.tugas;
 
 import android.os.Bundle;
-import android.os.Process;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
 
-    RadioGroup rgbaru;
-    TextView tvHasil;
+public class MainActivity extends AppCompatActivity implements {
     EditText etNama;
-    CheckBox cbMedsan;
-    CheckBox cbMac;
-    CheckBox cbTsbc;
-    Button bDt;
-    Spinner spB;
+    RadioGroup rgBaru;
+    Spinner spYa;
+    CheckBox cbM, cbMa, cbTs;
+    Button bD;
+    TextView tvH;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        etNama = (EditText) findViewById(R.id.etNama);
-        rgbaru = (RadioGroup) findViewById(R.id.rgbaru);
-        tvHasil = (TextView) findViewById(R.id.tvHasil);
-        bDt.setOnClickListener(new View.OnClickListener() {
-            findViewById(R.id.bDt)
+        etNama = (EditText) findViewById(R.id.EditText);
+        rgBaru = (RadioGroup) findViewById(R.id.rgbaru);
+        spYa = (Spinner) findViewById(R.id.spB);
+        cbM = (CheckBox) findViewById(R.id.cbMedsan);
+        cbMa = (CheckBox) findViewById(R.id.cbMac);
+        cbTs = (CheckBox) findViewById(R.id.cbTsbc);
+        cbM.setOnCheckedChangeListener(this);
+        cbMa.setOnCheckedChangeListener(this);
+        cbTs.setOnCheckedChangeListener(this);
 
-            setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick (View view){
-                    doClick();
-                }
-            }
+        bD = (Button) findViewById(R.id.bDt);
+        tvH = (TextView) findViewById(R.id.tvHasil);
 
-            .
-
+        bD.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                do Process();
+            public void onClick(View v) {
+                doClick();
             }
-
-            )
-        };
+        });
+    }
 
     private void doClick() {
-        String tvhasil = null;
+        String nama = etNama.getText().toString();
+        String hasil = null;
+        String kelas = spYa.getSelectedItem().toString();
 
-        if (tvHasil == null) {
-            tvhasil.setText("Belum Memilih Kelas");
+        if (rgBaru.getCheckedRadioButtonId() != -1) {
+            RadioButton rb = (RadioButton)
+                    findViewById(rgBaru.getCheckedRadioButtonId());
+            hasil = rb.getText().toString();
+        }
+
+        if (nama.isEmpty()) {
+            etNama.setError("Nama Belum Diisi");
+        } else if (nama.length() < 3) {
+            etNama.setError("Nama Minimal 3 Karakter");
         } else {
-            tvhasil.setText(etNama + "Kelas anda " + tvhasil);
+            etNama.setError(null);
         }
+        tvH.setText("--------------- ( METIC ) ---------------" + "\n Terima Kasih" + nama + "\n dariKelas : " + kelas + "\n Kelas Metic Kamu :" + hasil);
     }
-});
 
-private void doPocess(){
-        if(isValid())
-        {String nama=etNama.getText().toString();
-        tvHasil.setText("Terima Kasih "+nama+"terdaftar di kelas"+tvhasil);
-        }
-        }
-
-private boolean isValid(){
-        boolean valid=true;
-
-        String nama=etNama.getText().toString();
-
-        if(nama.isEmpty())
-        {
-        etNama.setError("Nama Belum Diisi");
-        valid=false;
-        }
-        else
-        {
-        etNama.setError(null);
-        }
-        return valid;
-        }
-        }
-        }
+}
 
 
